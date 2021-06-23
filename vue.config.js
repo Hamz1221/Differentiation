1f40d2ef5d1e9a31bff069b30de17633fdd169cc
Hamzah <h4mz1221@gmail.com>
2021-06-23 14:06:15 +0100

    My first Commit

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    publicPath:'./',
+    pages:{
+        index:{
+            entry:'src/main.js',
+            title:'Index'
+        },
+            first:{
+                entry:'src/First/main.js',
+                title:'First',
+                
+            },
+            second:{
+                entry:'src/Second/main.js',
+                title:'Second',
+                
+            },
+    }
+}
